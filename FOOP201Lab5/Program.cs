70c2cfc286e894f9a572805ef7e00b708a05b768
Samuel Colville <S00182360@mail.itsligo.ie>
2018-11-05 12:20:59 +0000

    Begin class structure

--- FOOP201Lab5/Program.cs
+++ FOOP201Lab5/Program.cs
@@ -9,4 +9,26 @@ namespace FOOP201Lab5
             Console.WriteLine("Hello World!");
         }
     }
+
+    class Team
+    {
+        public Team(string _teamName)
+        {
+            TeamName = _teamName;
+        }
+        public Team()
+        {
+
+        }
+
+        private string teamName;
+        public string TeamName { get { return teamName; } }
+        private readonly int wins;
+        public int Wins { get { return wins; } }
+        private readonly int losses;
+        public int Losses { get { return losses; } }
+        private readonly int draws;
+        public int Draws { get { return draws; } }
+
+    }
 }

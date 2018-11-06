be34759e96284798541b5314b964e261ac6a753f
Samuel Colville <S00182360@mail.itsligo.ie>
2018-11-06 01:54:55 +0000

    Added details for each object in the array

--- FOOP201Lab5/Program.cs
+++ FOOP201Lab5/Program.cs
@@ -6,7 +6,25 @@ namespace FOOP201Lab5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string formatString = "{0,-15}{1,-8}{2,-8}{3,-8}{4,-8}{5,-8}";
+            Team SligoRovers = new Team("Sligo  Rovers");
+            Team FinnHarps = new Team("Finn  Harps");
+            Team GalwayUnited = new Team("Galway  United");
+            Team DerryCity = new Team("Derry  City");
+            Team Dundalk = new Team("Dundalk");
+            Team[] teams = new Team[5];
+            teams[0] = SligoRovers;
+            teams[1] = FinnHarps;
+            teams[2] = GalwayUnited;
+            teams[3] = DerryCity;
+            teams[4] = Dundalk;
+            Console.WriteLine(formatString, "Team Name", "Points", "Wins", "Draws", "Losses", "Played");
+            foreach (var te in teams)
+            {
+                Console.WriteLine(formatString, te.TeamName, te.Points, te.Wins, te.Draws, te.Losses, te.Games);
+            }
+
+            Console.ReadKey();
         }
     }
 
@@ -14,7 +32,7 @@ namespace FOOP201Lab5
     {
         public Team(string _teamName)
         {
-            TeamName = _teamName;
+            teamName = _teamName;
         }
         public Team()
         {
@@ -29,6 +47,10 @@ namespace FOOP201Lab5
         public int Losses { get { return losses; } }
         private readonly int draws;
         public int Draws { get { return draws; } }
+        private readonly int points;
+        public int Points { get { return points; } }
+        private readonly int games;
+        public int Games { get { return games; } }
 
     }
 }
